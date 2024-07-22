3ac0767094e076ed299f3da293c2642cd7474fc2
hyomin1 <leehm123456@gmail.com>
2024-07-22 20:02:34 +0900

    WebSocket 통한 서버 브라우저 메시지 주고받기

--- src/server.js
+++ src/server.js
@@ -1,13 +1,35 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+// http 서버
+const server = http.createServer(app);
+
+// websocket서버
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+  console.log(socket);
+}
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from Browser"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf-8"));
+  });
+  socket.send("hello!!!");
+});
+
+// http와 ws 동시에 돌릴 수 있음
+
+server.listen(3000, handleListen);
